2cb5fe276f5710c8740563f7a4428e5ab000c4aa
munezeromicha <munezerontaganiramichel@gmail.com>
2025-05-05 10:15:15 +0200

    fix-attendees-errors

---
+++ src/services/attendeeService.ts
@@ -0,0 +1,26 @@
+import { AppDataSource } from "../config/database";
+import { Attendee } from "../models/Attendee";
+
+const attendeeRepository = AppDataSource.getRepository(Attendee);
+
+export const getAllAttendees = async (): Promise<Attendee[]> => {
+  return await attendeeRepository.find();
+};
+
+export const getAttendeeById = async (id: string): Promise<Attendee | null> => {
+  return await attendeeRepository.findOne({ where: { attendeeId: id } });
+};
+
+export const createAttendee = async (attendeeData: Partial<Attendee>): Promise<Attendee> => {
+  const attendee = attendeeRepository.create(attendeeData);
+  return await attendeeRepository.save(attendee);
+};
+
+export const updateAttendee = async (id: number, attendeeData: Partial<Attendee>): Promise<Attendee | null> => {
+  await attendeeRepository.update(id, attendeeData);
+  return await getAttendeeById(id.toString());
+};
+
+export const deleteAttendee = async (id: number): Promise<void> => {
+  await attendeeRepository.delete(id);
+};
